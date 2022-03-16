9d3d9f72048792ff91120abf20a3c74b3a16e193
ljcucc <ljcucc@gmail.com>
2022-03-16 20:39:35 +0800

    .

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ export const router = new Router(outlet);
 import "./pages/Home.page.js";
 import "./pages/About.page.js";
 import "./pages/Profile.page.js";
+import "./pages/Dashboard.page.js";
+
 import "./components/Topbar.js"
 // import "./pages/About.page.js";
 // import "./pages/NotFound.page.js";
@@ -17,5 +19,6 @@ router.setRoutes([
   { path: '/about', component: 'about-page-comp' },
   // {path: '/users/:user', component: 'x-user-profile'},
   {path: '/profile/:id', component: 'profile-page-comp'},
+  {path: '/dashboard', component: 'dashboard-page'},
   { path: '(.*)', component: '404-page-comp' },
 ]);
